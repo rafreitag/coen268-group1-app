c803a87cbb94b5de46ceda808e3671377fd41a27
agigliotti24 <agigliotti24@gmail.com>
2020-11-20 20:16:04 -0800

    Added ability to make and store an account with Firebase

--- FitnessMatch/app/src/main/java/com/example/fitnessmatch/SignUpActivity.java
+++ FitnessMatch/app/src/main/java/com/example/fitnessmatch/SignUpActivity.java
@@ -7,9 +7,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
     public final static String TAG = "SignUpActivity";
@@ -17,6 +25,9 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText et_name;
     private EditText et_email;
     private EditText et_password;
+    private EditText confirm_password;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +37,16 @@ public class SignUpActivity extends AppCompatActivity {
         et_name = findViewById(R.id.et_name);
         et_email = findViewById(R.id.et_email);
         et_password = findViewById(R.id.et_password);
+        confirm_password = findViewById(R.id.confirm_password);
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
     }
 
     public void add_user(View view) {
         String name = et_name.getText().toString();
         String email = et_email.getText().toString();
         String password = et_password.getText().toString();
+        String password_confirm = confirm_password.getText().toString();
 
         if (name.isEmpty()) {
             Toast.makeText(this, "Name cannot be empty.", Toast.LENGTH_SHORT).show();
@@ -45,25 +60,54 @@ public class SignUpActivity extends AppCompatActivity {
             Toast.makeText(this, "Password cannot be empty.", Toast.LENGTH_SHORT).show();
             return;
         }
-        // 3: Add the record to the database
-        UserDbHelper dbHelper = new UserDbHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(UserContractInfo.Users.USER_NAME, name);
-        contentValues.put(UserContractInfo.Users.USER_EMAIL, email);
-        contentValues.put(UserContractInfo.Users.USER_PASSWORD, password);
-
-        long recordId = db.insert(UserContractInfo.Users.TABLE_NAME, null, contentValues);
-        db.close();
-
-        if(recordId == -1) {
-            Toast.makeText(this, "Insertion failed", Toast.LENGTH_SHORT).show();
-        } else {
-            Log.i(TAG, "Successfully added user " + name + " in the db");
-            Intent intent = new Intent(this, HomeActivity.class);
-            intent.putExtra("Name", name);
-            startActivity(intent);
+        if(!password_confirm.equals(password)){
+            Toast.makeText(this, "Password mismatch.", Toast.LENGTH_SHORT).show();
+            return;
         }
+        progressBar.setVisibility(view.VISIBLE);
+
+        // Create user in Firebase
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    // NEEDS TO CHANGE... THIS IS JUST FOR TESTING PURPOSES... THIS SHOULD PROBABLY INSTEAD ACTIVATE AN ACTIVITY
+                    setContentView(R.layout.find);
+                }
+                else{
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+//                    progressBar.setVisibility(view.INVISIBLE);
+                }
+            }
+        });
+
+//        // 3: Add the record to the database
+//        UserDbHelper dbHelper = new UserDbHelper(this);
+//        SQLiteDatabase db = dbHelper.getWritableDatabase();
+//
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put(UserContractInfo.Users.USER_NAME, name);
+//        contentValues.put(UserContractInfo.Users.USER_EMAIL, email);
+//        contentValues.put(UserContractInfo.Users.USER_PASSWORD, password);
+//
+//        long recordId = db.insert(UserContractInfo.Users.TABLE_NAME, null, contentValues);
+//        db.close();
+//
+//        if(recordId == -1) {
+//            Toast.makeText(this, "Insertion failed", Toast.LENGTH_SHORT).show();
+//        } else {
+//            Log.i(TAG, "Successfully added user " + name + " in the db");
+//            Intent intent = new Intent(this, HomeActivity.class);
+//            intent.putExtra("Name", name);
+//            startActivity(intent);
+//        }
+    }
+
+    public void back_to_login(View view){
+        Intent a = new Intent(this, LoginActivity.class);
+        startActivity(a);
     }
 }
