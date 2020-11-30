cd774e6d02ebd778742bbadcb67442450b89b686
Rachael Freitag <rafreitag16@gmail.com>
2020-11-29 22:34:01 -0800

    Edited match form layouts

---
+++ FitnessMatch/app/src/main/java/com/example/fitnessmatch/Find3Activity.java
@@ -0,0 +1,82 @@
+//  I FEEL LIKE THE SECOND PAGE FOR FIND SHOULD BE A FRAGMENT
+//  AND PART OF THE FIRST ACTIVITY IN SOME WAY BUT I HONESTLY
+//  DON'T KNOW HOW TO IMPLEMENT FRAGMENTS AT ALL SO FOR NOW IT IS
+//  SIMPLY ANOTHER ACTIVITY. WE CAN MAYBE SEE IF WE CAN CHANGE THIS
+//  BEFORE WE TURN IN THE FINAL PRODUCT BUT FOR NOW I THINK THIS
+//  TECHNICALLY WORKS SO IM NOT GOING TO STRESS OVER IT.
+
+package com.example.fitnessmatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.Serializable;
+
+public class Find3Activity extends AppCompatActivity {
+    private RadioButton pref_time1, pref_time2, pref_time3, pref_time4, pref_time5;
+//    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    private Preferences preferences;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_find3);
+        mAuth = FirebaseAuth.getInstance();
+//        progressBar = findViewById(R.id.progressBar3);
+
+        // Receive intent passed in from FindActivity
+        Intent a = getIntent();
+        // Receive preferences object passed in from FindActivity (includes all user filled out preferences from page 1)
+        preferences = (Preferences) a.getSerializableExtra("preferences");
+
+        // What time of day do you like to work out?
+        pref_time1 = findViewById(R.id.rb_pref_time_1);
+        pref_time2 = findViewById(R.id.rb_pref_time_2);
+        pref_time3 = findViewById(R.id.rb_pref_time_3);
+        pref_time4 = findViewById(R.id.rb_pref_time_4);
+        pref_time5 = findViewById(R.id.rb_pref_time_5);
+    }
+
+    // Set all preferences (for page 2)
+    public void update_preferences(){
+        // Set pref_time
+        if(pref_time1.isChecked())
+            preferences.setPref_time(1);
+        else if(pref_time2.isChecked())
+            preferences.setPref_time(2);
+        else if(pref_time3.isChecked())
+            preferences.setPref_time(3);
+        else if(pref_time4.isChecked())
+            preferences.setPref_time(4);
+        else if(pref_time5.isChecked())
+            preferences.setPref_time(5);
+    }
+
+    // NEED TO RETRIEVE ALL USER DATA AT THIS POINT SO WE CAN DO THE MATCH STUFF BUT I HAVE NOT QUITE FIGURED OUT HOW TO DO THAT
+    // THE CODE TO RETRIEVE ONE SPECIFIC USER PROFILE IS IN HOMEACTIVITY.JAVA
+    // Adds all preferences (from page 1 and page 2 of find) to Firebase and then redirects to home page
+    public void nextPage(View view) {
+        // Create intent to go to second page of find
+        Intent a = new Intent(this, Find4Activity.class);
+        // Update preferences object with all of the current preferences filled out during page 1
+        update_preferences();
+        // Sending this preferences object to the second find page so that none of the choices are lost
+        a.putExtra("preferences", (Serializable) preferences);
+        // Going to page 2
+        startActivity(a);
+    }
+}
