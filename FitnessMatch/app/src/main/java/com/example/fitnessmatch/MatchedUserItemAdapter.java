99c3421dfb7e7f62831b12d5b71397ea7bc48033
chrislayco <57198098+chrislayco@users.noreply.github.com>
2020-12-01 12:58:22 -0800

    changes in progress

--- FitnessMatch/app/src/main/java/com/example/fitnessmatch/MatchedUserItemAdapter.java
+++ FitnessMatch/app/src/main/java/com/example/fitnessmatch/MatchedUserItemAdapter.java
@@ -78,6 +78,7 @@ public class MatchedUserItemAdapter extends ArrayAdapter<MatchedUserItem> {
                 sendRequestTo(user_id);
                 //verification button
                 //change color and stuff
+                //btn_send_request.setBa;
                 //toast?
             }
         });
