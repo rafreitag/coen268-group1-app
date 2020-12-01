c0e94cd008e88e4e2e7fc0a68404b7b23d8468a0
chrislayco <57198098+chrislayco@users.noreply.github.com>
2020-12-01 01:45:12 -0800

    fixed bug where match list would not display on clean install
    moved database generation and list population into fragment

--- FitnessMatch/app/src/main/java/com/example/fitnessmatch/FindListFragment.java
+++ FitnessMatch/app/src/main/java/com/example/fitnessmatch/FindListFragment.java
@@ -1,5 +1,6 @@
 package com.example.fitnessmatch;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -14,6 +15,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,19 +42,25 @@ public class FindListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_find_list, container, false);
         Log.i("LIST_FRAG", "fragment created");
-
         listView = view.findViewById(R.id.listViewUser);
+        createMatchDatabase();
+
+        return view;
+    }
+
+    public void populateListArray(UserMatchDataDBHelper dbHelper){
+
 
         ArrayList<MatchedUserItem> matchedUserList = new ArrayList<>();
 
-        UserMatchDataDBHelper dbHelper = new UserMatchDataDBHelper(getActivity());
+
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
         //CAN DO FILTERING HERE EVENTUALLY INSTEAD OF QUERYING ALL
         String order = "match_score";
 
         Cursor cursor = db.rawQuery("SELECT * FROM " + UserMatchDataContract.MatchData.TABLE_NAME +
-                                        " ORDER BY CAST(" + order + " AS INTEGER) DESC", null);
+                " ORDER BY CAST(" + order + " AS INTEGER) DESC", null);
 
         cursor.moveToFirst();
 
@@ -76,7 +91,62 @@ public class FindListFragment extends Fragment {
 
         MatchedUserItemAdapter matchedUserItemAdapter = new MatchedUserItemAdapter(getActivity(), R.layout.matched_user_item, matchedUserList);
         listView.setAdapter(matchedUserItemAdapter);
+    }
 
-        return view;
+    public void createMatchDatabase(){
+
+        String TAG = "createMatchDatabase";
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        UserMatchDataDBHelper userMatchDataDBHelper = new UserMatchDataDBHelper(getActivity());
+
+        DatabaseReference myDatabase = FirebaseDatabase.getInstance().getReference("users");
+
+        myDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                userMatchDataDBHelper.clearData();
+
+                Preferences currentUserPreferences = snapshot.child(mAuth.getCurrentUser().getUid()).child("preferences").getValue(Preferences.class);
+
+                for (DataSnapshot iterateSnapshot : snapshot.getChildren()) {
+
+
+                    String user_id = iterateSnapshot.getKey();
+
+                    //Log.i(TAG, user_id +  " compared with " + mAuth.getCurrentUser().getUid());
+
+                    if(!user_id.equals(mAuth.getCurrentUser().getUid())) {
+
+
+                        Preferences preference = iterateSnapshot.child("preferences").getValue(Preferences.class);
+                        Profile profile = iterateSnapshot.child("profile").getValue(Profile.class);
+
+
+                        String user_name = profile.getName();
+
+                        int match_score = currentUserPreferences.calculateMatchScore(preference);
+                        double distance = currentUserPreferences.calculateDistanceFrom(preference);
+
+//                        Log.i(TAG, "COMPARED WITH USER: " + iterateSnapshot.child("profile").getValue(Profile.class).getName() +
+//                                "\nMatch Score = " + match_score +
+//                                "\nDistance = " + String.format("%.2f", distance));
+
+                        userMatchDataDBHelper.addData(user_id, user_name, String.format("%.2f", distance), String.valueOf(match_score));
+                    }
+                }
+                populateListArray(userMatchDataDBHelper);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "Read failed. Please retry login", Toast.LENGTH_LONG).show();
+                // Logout User
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- FitnessMatch/app/src/main/java/com/example/fitnessmatch/NewFindActivity.java
+++ FitnessMatch/app/src/main/java/com/example/fitnessmatch/NewFindActivity.java
@@ -20,28 +20,12 @@ import com.google.firebase.firestore.auth.User;
 public class NewFindActivity extends AppCompatActivity {
 
     //Preferences currentUserPreferences; // = new Preferences(true, false, true, false, true, false, true, false, true, false, true, false, true, 1, 2, 3, 96.212, -121.323, "other");
-    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_find);
-        mAuth = FirebaseAuth.getInstance();
-
-        //move to home activity
-        createMatchDatabase();
-
-        //check if local database has anything
-        //if it does, populate list
-        //if none, make popup, go to preferences activity
-
-//        if (true) {
-//            createMatchDatabase();
-//        }
-//        else{
-//            //popup saying that you need to fill out your preferences first
-//            //takes you to filling out preferences page
-//        }
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -53,60 +37,7 @@ public class NewFindActivity extends AppCompatActivity {
 
 
 
-    public void createMatchDatabase(){
 
-        String TAG = "createMatchDatabase";
-        UserMatchDataDBHelper userMatchDataDBHelper = new UserMatchDataDBHelper(this);
-
-        DatabaseReference myDatabase = FirebaseDatabase.getInstance().getReference("users");
-
-        myDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                userMatchDataDBHelper.clearData();
-
-
-                Preferences currentUserPreferences = snapshot.child(mAuth.getCurrentUser().getUid()).child("preferences").getValue(Preferences.class);
-
-                for (DataSnapshot iterateSnapshot : snapshot.getChildren()) {
-
-
-                    String user_id = iterateSnapshot.getKey();
-
-                    //Log.i(TAG, user_id +  " compared with " + mAuth.getCurrentUser().getUid());
-
-                    if(!user_id.equals(mAuth.getCurrentUser().getUid())) {
-
-
-                        Preferences preference = iterateSnapshot.child("preferences").getValue(Preferences.class);
-                        Profile profile = iterateSnapshot.child("profile").getValue(Profile.class);
-
-
-                        String user_name = profile.getName();
-
-                        int match_score = currentUserPreferences.calculateMatchScore(preference);
-                        double distance = currentUserPreferences.calculateDistanceFrom(preference);
-
-//                        Log.i(TAG, "COMPARED WITH USER: " + iterateSnapshot.child("profile").getValue(Profile.class).getName() +
-//                                "\nMatch Score = " + match_score +
-//                                "\nDistance = " + String.format("%.2f", distance));
-
-                        userMatchDataDBHelper.addData(user_id, user_name, String.format("%.2f", distance), String.valueOf(match_score));
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(NewFindActivity.this, "Read failed. Please retry login", Toast.LENGTH_LONG).show();
-                // Logout User
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(NewFindActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
 
 
 }
